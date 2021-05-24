0dec2c5bc3b1a393e44411a79158382356587e30
rainty02 <rainty02@gmail.com>
2021-05-24 18:36:40 +0900

    Create Member.java
    이름, 나이, 키, 책의 보유 여부

---
+++ java.project/MyProject/src/ch02/Member.java
@@ -0,0 +1,13 @@
+package ch02;
+
+public class Member {
+
+	public static void main(String[] args) {
+		String name = "Lee";
+		int age = 10;
+		double height = 100.1;
+		boolean hasBook = true;
+		
+		System.out.println("이름은 " + name + ", " + "나이는 " + age + ", " + "키는 " + height + ", " + "책의 보유 여부 : " + hasBook);
+	}
+}
