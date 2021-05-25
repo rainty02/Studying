7f54b97b6b5fa764e29f1af6d55ba113a71b6bde
rainty02 <rainty02@gmail.com>
2021-05-25 16:16:06 +0900

    Update Member.java

--- java.project/MyProject/src/ch04/Member.java
+++ java.project/MyProject/src/ch04/Member.java
@@ -5,10 +5,11 @@ import java.util.Scanner;
 
 public class Member {
 	
+	Calendar cal = Calendar.getInstance();
+	int year = cal.get(Calendar.YEAR);
+	// 현재 연도 가져오기 
+	
 	void inoculation(int birth) {
-		Calendar cal = Calendar.getInstance();
-		int year = cal.get(Calendar.YEAR);
-		// 현재 연도 가져오기 
 		int result = year - birth;
 		if( result < 15 || result >= 65) {
 			System.out.println("무료예방접종이 가능합니다.");
@@ -24,7 +25,7 @@ public class Member {
 				System.out.println("홀수 해에 건강검진을 받을 수 있습니다.");
 			}
 		}
-		if(birth >= 40) {
+		if(year-birth >= 40) {
 			System.out.println("암검진 대상입니다.");
 		}
 	}	
