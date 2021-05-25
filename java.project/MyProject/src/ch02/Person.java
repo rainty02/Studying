b3e8cdc232f473aa775f1a917b6afd85222a24a5
rainty02 <rainty02@gmail.com>
2021-05-25 11:18:13 +0900

    Update Person.java

--- java.project/MyProject/src/ch02/Person.java
+++ java.project/MyProject/src/ch02/Person.java
@@ -24,8 +24,6 @@ public class Person {
 		
 		System.out.printf("이름 : %s\n전화번호 : %s\n주민등록번호 : %s",per.perName, per.perTel, per.perNo);
 		System.out.println();
-		per.print();
-		
+		per.print();		
 	}
-
 }
