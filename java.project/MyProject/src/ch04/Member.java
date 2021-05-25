2060e9f48f3726200bf6046894ebfe69eac66c3c
rainty02 <rainty02@gmail.com>
2021-05-25 16:13:50 +0900

    Update Member.java

--- java.project/MyProject/src/ch04/Member.java
+++ java.project/MyProject/src/ch04/Member.java
@@ -5,11 +5,10 @@ import java.util.Scanner;
 
 public class Member {
 	
-	Calendar cal = Calendar.getInstance();
-	int year = cal.get(Calendar.YEAR);
-	// 현재 연도 가져오기 
-	
 	void inoculation(int birth) {
+		Calendar cal = Calendar.getInstance();
+		int year = cal.get(Calendar.YEAR);
+		// 현재 연도 가져오기 
 		int result = year - birth;
 		if( result < 15 || result >= 65) {
 			System.out.println("무료예방접종이 가능합니다.");
@@ -18,15 +17,14 @@ public class Member {
 		}
 	}
 	void checkup(int birth) {
-		int result = year - birth;
-		if(result >= 20) {
-			if(result%2 != 0) {
+		if(birth >= 20) {
+			if(birth%2 == 0) {
 				System.out.println("짝수 해에 건강검진을 받을 수 있습니다.");
 			} else {
 				System.out.println("홀수 해에 건강검진을 받을 수 있습니다.");
 			}
 		}
-		if(result >= 40) {
+		if(birth >= 40) {
 			System.out.println("암검진 대상입니다.");
 		}
 	}	
