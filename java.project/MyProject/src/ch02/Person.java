c06483bb0e60133602aca5edc68961e196b6e6fa
rainty02 <rainty02@gmail.com>
2021-05-25 11:10:11 +0900

    출력 메소드 추가

--- java.project/MyProject/src/ch02/Person.java
+++ java.project/MyProject/src/ch02/Person.java
@@ -6,6 +6,15 @@ public class Person {
 	String perTel;
 	String perNo;
 	
+	void print() {
+		System.out.print("이름: ");
+		System.out.println(perName);
+		System.out.print("나이: ");
+		System.out.println(perTel);
+		System.out.print("주민등록번호: ");
+		System.out.println(perNo);
+	}
+	
 	public static void main(String[] args) {
 		
 		Person per = new Person();
@@ -14,6 +23,9 @@ public class Person {
 		per.perNo = "000000-0000000";
 		
 		System.out.printf("이름 : %s\n전화번호 : %s\n주민등록번호 : %s",per.perName, per.perTel, per.perNo);
+		System.out.println();
+		per.print();
+		
 	}
 
 }
