8976837fbaa83548de98a10e3ebf6e85a6b05a3f
rainty02 <rainty02@gmail.com>
2021-05-26 17:19:54 +0900

    chap07

--- java.project/MyProject/src/ch07/Female.java
+++ java.project/MyProject/src/ch07/Female.java
@@ -3,8 +3,8 @@ package ch07;
 public class Female extends Person {
 //	2. Person 클래스를 상속해서 확장하는 새로운 클래스 Male 클래스와 Female 클래스를 정의 해봅시다. 
 //	① 각 클래스는 상속 받은 멤버 외에 추가적인 변수와 메소드를 추가해서 새로운 클래스를 정의해봅시다.
-	String name;
-	int perNo;	
+//	String name;
+//	int perNo;	
 	String job;
 	
 	public Female(String name, int perNo,String job) {
